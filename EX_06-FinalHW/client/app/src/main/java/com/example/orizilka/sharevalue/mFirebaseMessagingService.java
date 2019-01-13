beac1956051d7d70b334eba97bb209c373ee7fb3
Ori Zilka <zilkaori@gmail.com>
2019-01-13 23:15:00 +0200

    firebase services added

---
+++ EX_06-FinalHW/client/app/src/main/java/com/example/orizilka/sharevalue/mFirebaseMessagingService.java
@@ -0,0 +1,18 @@
+package com.example.orizilka.sharevalue;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+
+public class mFirebaseMessagingService extends FirebaseMessagingService {
+    public mFirebaseMessagingService() {
+    }
+
+//    @Override
+//    public IBinder onBind(Intent intent) {
+//        // TODO: Return the communication channel to the service.
+//        throw new UnsupportedOperationException("Not yet implemented");
+//    }
+}
